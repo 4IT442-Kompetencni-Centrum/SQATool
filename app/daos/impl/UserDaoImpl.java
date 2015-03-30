302e4df4a0bacda57dce753aef9e889a74ab68e8
pedro-cze <petrkadlec.net@gmail.com>
2015-03-30 23:45:29 +0200

    main template + some minor changes

--- app/daos/impl/UserDaoImpl.java
+++ app/daos/impl/UserDaoImpl.java
@@ -4,6 +4,8 @@ import daos.UserDao;
 import models.User;
 import play.db.jpa.JPA;
 
+import javax.persistence.TypedQuery;
+
 /**
  * Created by Petr Kadlec on 29.3.2015.
  */
@@ -15,6 +17,10 @@ public class UserDaoImpl extends AbstractDaoImpl<User> implements UserDao{
 
     @Override
     public User getValidUser(String userName, String password) {
-        return null; //TODO
+        String queryString = "select * from SQA_USER where USERNAME like :username and PASSWORD like :password";
+        TypedQuery<User> query = JPA.em().createQuery(queryString, User.class);
+        query.setParameter("username", userName);
+        query.setParameter("password", password);
+        return query.getSingleResult();
     }
 }
