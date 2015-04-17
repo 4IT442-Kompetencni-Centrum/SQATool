623cda1ca471f0f7342a70eed2d3e2cd186a5fe6
Tomas Michalicka <tomas@michalicka.com>
2015-04-17 11:21:20 +0200

    fix of Mira's version

---
+++ app/controllers/UserController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class UserController extends Controller {
+
+	public static Result show(Long id) {
+		//TODO mcech
+		return ok();
+	}
+	
+	/**
+	 * Action returns JSON array for ajax calls (autocomplete)
+	 * @param query
+	 * @return
+	 */
+	@Transactional(readOnly=true)
+	public static Result getUserByNamePattern(String query) {
+		//TODO tmichalicka
+		return ok();
+	}
+}
