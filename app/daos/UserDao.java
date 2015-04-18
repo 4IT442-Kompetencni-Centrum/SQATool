a4d325371f365e49e83961e64030fb0a63a730af
Tomas Michalicka <tomas@michalicka.com>
2015-04-18 17:38:24 +0200

    fix

--- app/daos/UserDao.java
+++ app/daos/UserDao.java
@@ -1,5 +1,7 @@
 package daos;
 
+import java.util.Map;
+
 import models.User;
 
 /**
--- app/models/User.java
+++ app/models/User.java
@@ -41,7 +41,75 @@ public class User extends AbstractVersionedEntity {
         return firstName + " " + lastName;
     }
 
-    public static class Page{
+    
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public StateUser getStateUser() {
+		return stateUser;
+	}
+
+	public void setStateUser(StateUser stateUser) {
+		this.stateUser = stateUser;
+	}
+
+
+
+	public static class Page{
 
         private final int pageSize;
         private final long totalRowCount;
