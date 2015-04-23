e35c1b582daeedeb03928b971e01093c57640131
Aleš Jiránek <a.jiranek@gmail.com>
2015-04-23 11:14:56 +0200

    RewardsModule: updated security service according to use case diagram

--- app/service/SecurityService.java
+++ app/service/SecurityService.java
@@ -230,35 +230,35 @@ public class SecurityService {
 	private static void initRewardAccessMap() {
 		HashMap<String, Boolean> showAll = new HashMap<>();
 		showAll.put(EnumerationWithKeys.MEMBER_KEY, false);
-		showAll.put(EnumerationWithKeys.MANAGER_KC_KEY, true);
+		showAll.put(EnumerationWithKeys.MANAGER_KC_KEY, false);
 		showAll.put(EnumerationWithKeys.HEAD_KC_KEY, true);
 		showAll.put(EnumerationWithKeys.ADMIN_KEY, true);
 		accessMap.put(ActionsEnum.REWARD_SHOW_ALL, showAll);
 
 		HashMap<String, Boolean> show = new HashMap<>();
 		show.put(EnumerationWithKeys.MEMBER_KEY, false);
-		show.put(EnumerationWithKeys.MANAGER_KC_KEY, true);
+		show.put(EnumerationWithKeys.MANAGER_KC_KEY, false);
 		show.put(EnumerationWithKeys.HEAD_KC_KEY, true);
 		show.put(EnumerationWithKeys.ADMIN_KEY, true);
 		accessMap.put(ActionsEnum.REWARD_SHOW, show);
 
 		HashMap<String, Boolean> add = new HashMap<>();
 		add.put(EnumerationWithKeys.MEMBER_KEY, false);
-		add.put(EnumerationWithKeys.MANAGER_KC_KEY, true);
+		add.put(EnumerationWithKeys.MANAGER_KC_KEY, false);
 		add.put(EnumerationWithKeys.HEAD_KC_KEY, true);
 		add.put(EnumerationWithKeys.ADMIN_KEY, true);
 		accessMap.put(ActionsEnum.REWARD_ADD, add);
 
 		HashMap<String, Boolean> edit = new HashMap<>();
 		edit.put(EnumerationWithKeys.MEMBER_KEY, false);
-		edit.put(EnumerationWithKeys.MANAGER_KC_KEY, true);
+		edit.put(EnumerationWithKeys.MANAGER_KC_KEY, false);
 		edit.put(EnumerationWithKeys.HEAD_KC_KEY, true);
 		edit.put(EnumerationWithKeys.ADMIN_KEY, true);
 		accessMap.put(ActionsEnum.REWARD_EDIT, edit);
 
 		HashMap<String, Boolean> delete = new HashMap<>();
 		delete.put(EnumerationWithKeys.MEMBER_KEY, false);
-		delete.put(EnumerationWithKeys.MANAGER_KC_KEY, true);
+		delete.put(EnumerationWithKeys.MANAGER_KC_KEY, false);
 		delete.put(EnumerationWithKeys.HEAD_KC_KEY, true);
 		delete.put(EnumerationWithKeys.ADMIN_KEY, true);
 		accessMap.put(ActionsEnum.REWARD_DELETE, delete);
