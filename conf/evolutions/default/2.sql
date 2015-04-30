54e6cdca759d6f32ab0343ed3eb636e48e034a01
Tomas Michalicka <tomas@michalicka.com>
2015-04-30 14:41:20 +0200

    evolutions split

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,16 @@
+# --- !Ups
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(1, 0, TRUE, 'Jan', 'Admin', 'admin', 'admin');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(2, 0, TRUE, 'Karel', 'Vedouci', 'head', 'head');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(3, 0, TRUE, 'Filip', 'Manazer', 'manager', 'manager');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(4, 0, TRUE, 'Dominik', 'Clen', 'member', 'member');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(5, 0, TRUE, 'Tomas', 'Michalicka', 'tomas', 'tomas');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(6, 0, TRUE, 'Ales', 'Jiranek', 'ales', 'ales');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(7, 0, TRUE, 'Petr', 'Kadlec', 'petr', 'petr');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(8, 0, TRUE, 'Miroslav', 'Cech', 'miroslav', 'miroslav');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(9, 0, TRUE, 'Jiri', 'Siroky', 'jiri', 'jiri');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(10, 0, TRUE, 'Ondrej', 'Kostal', 'ondrej', 'ondrej');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(11, 0, TRUE, 'Radek', 'Slavetinsky', 'radek', 'radek');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(12, 0, TRUE, 'Vladimir', 'Rahm', 'vladimir', 'vladimir');
+INSERT INTO SQA_USER (id, version, visible, firstname, lastname, password, username) VALUES(13, 0, TRUE, 'Jan', 'Smetana', 'jan', 'jan');
+
+# --- !Downs
